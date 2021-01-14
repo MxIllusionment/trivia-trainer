8f6afcd201e21a954f41a5ea05ebb69bc8156379
Steven Israel <malenchite@gmail.com>
2021-01-13 19:51:38 -0500

    Added function to generate bot avatar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,3 +2,8 @@
 function genUserAvatar(seed) {
   return "https://avatars.dicebear.com/api/gridy/" + seed + ".svg?colorful=1&deterministic=1";
 }
+
+/* Returns a link to a bot avatar image */
+function genBotAvatar(seed) {
+  return "https://avatars.dicebear.com/api/bottts/" + seed + ".svg?colorful=1&mouthChance=50&sidesChance=50&topChance=50";
+}
